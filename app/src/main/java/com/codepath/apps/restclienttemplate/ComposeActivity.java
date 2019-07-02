43b9ca06ca8b84b6bb23e08cc342a071c60e3f98
Elana Hummel <ehummel@fb.com>
2019-07-02 16:03:44 -0700

    implemented character counter when composing tweet

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -3,10 +3,13 @@ package com.codepath.apps.restclienttemplate;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.codepath.apps.restclienttemplate.models.User;
@@ -23,6 +26,7 @@ public class ComposeActivity extends AppCompatActivity {
     Button btnSend;
     EditText etTweet;
     TwitterClient client;
+    TextView tvCharacterCounter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +35,7 @@ public class ComposeActivity extends AppCompatActivity {
 
         etTweet = findViewById(R.id.etTweetBody);
         btnSend = findViewById(R.id.btnPost);
+        tvCharacterCounter = findViewById(R.id.tvCharacterCounter);
 
         btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,8 +44,23 @@ public class ComposeActivity extends AppCompatActivity {
             }
         });
 
+        etTweet.addTextChangedListener(mTextEditorWatcher);
+
         client = TwitterApp.getRestClient(this);
     }
+
+    private final TextWatcher mTextEditorWatcher = new TextWatcher() {
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            //This sets a textview to the current length
+            tvCharacterCounter.setText(String.valueOf(s.length()) + "/280");
+        }
+
+        public void afterTextChanged(Editable s) {
+        }
+    };
 
     public void sendTweet() {
         client.sendTweet(etTweet.getText().toString(), new AsyncHttpResponseHandler() {
